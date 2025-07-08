9045370f7f891fb2247e02a63c474dc1977c2a73
eliferezhenderson <elif.erez@gmail.com>
2025-07-08 18:48:53 +0200

    new

--- src/app/components/QuestionGenerator.js
+++ src/app/components/QuestionGenerator.js
@@ -142,7 +142,7 @@ export default function QuestionGenerator() {
             <h1 className="text-6xl font-semibold mt-0">HELLO, FRIEND</h1>
           </div>
           <p className="text-base max-w-4xl leading-snug text-vwRed">
-            This tool offers icebreaker questions for classrooms, teams, or groups of friends. You can press the buttons down below to get a new question.{' '}
+            This tool offers icebreaker questions for classrooms, teams, or groups of friends. You can press the buttons down below to get a new question.   {' '}
             <span className="underline font-medium">Go Random</span> to get a surprise, or try{' '}
             <span className="underline font-medium">Go Easier</span> for something light, or{' '}
             <span className="underline font-medium">Go Deeper</span> for a thoughtful conversation.
